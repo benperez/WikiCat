ba3b35ea5f0d96bb50691422ce9c4760cb3437d5
Stankalank <ben.prez@gmail.com>
2012-11-20 14:03:43 -0500

    added page class

--- Graph_Algorithms/src/Category.java
+++ Graph_Algorithms/src/Category.java
@@ -3,10 +3,7 @@
  * 
  * 
  * 
- * 
- * 
- * 
  **/
 public class Category {
-
+	
 }
--- Graph_Algorithms/src/Page.java
+++ Graph_Algorithms/src/Page.java
@@ -1,4 +1,31 @@
+import java.util.HashMap;
+import java.util.HashSet;
 
+
+/**
+ * Class for a Page, or Article, in Wikipedia
+ * 
+ * 
+ * 
+ * 
+ **/
 public class Page {
-
+	HashMap<Page,Double> TransitionProbabilities;
+	HashSet<Category> Categories;
+	
+	public Page(HashMap<Page,Double> TransitionProbabilities, HashSet<Category> Categories)
+	{
+		this.TransitionProbabilities = TransitionProbabilities;
+		this.Categories = Categories;
+	}
+	
+	public HashMap<Page,Double> getTransitionProbabilites()
+	{
+		return TransitionProbabilities;
+	}
+	
+	public HashSet<Category> getCategories()
+	{
+		return Categories;
+	}
 }
