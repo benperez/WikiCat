5dc5a3631b52362a865dea9efd662146f11f005e
Cris Feo <svgreaser@gmail.com>
2013-02-17 16:42:19 -0500

    Set up categories to retrieve their pages and moves sql statements over to using the new filtered tables.

--- Graph_Algorithms/src/graph/Category.java
+++ Graph_Algorithms/src/graph/Category.java
@@ -1,23 +1,70 @@
 package graph;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
- * 
- * 
- * 
+ * This class represents a wikipedia category.
  **/
 public class Category
 {
 	String name;
+	int article_count;
+	Set<Page> pages;
 	
 	/**
 	 * Basic Constructor for a Category
-	 * 
 	 * @param name the name of this Category
+	 * @param count the number of articles in this Category
 	 */
-	public Category(String name)
+	public Category(String name, int count)
 	{
 		this.name = name;
+		this.article_count = count;
 	}
+	
+	
+	/////////////////////////
+	// Data Retrieval
+	/////////////////////////
+	
+	/**
+	 * Queries the database to retrieve all pages in this category.
+	 */
+	private void retrievePages() {
+		String query =	"SELECT p.page_id as id " +
+						"FROM filtered_page AS p, filtered_categorylinks AS cl " +
+						"WHERE cl.cl_from=p.page_id AND cl.cl_to=" + name + ";" ;
+		Connection c = DBManager.getConnection();
+		ResultSet rs = DBManager.execute(c, query);
+		
+		if (rs != null)
+		{
+			pages = new HashSet<Page>();
+			
+			try 
+			{
+				while(rs.next())
+				{
+					pages.add( new Page(rs.getInt("id")) );
+				}
+			} catch (SQLException e)
+			{
+				e.printStackTrace();
+			}
+		}
+		
+		//Release the connection back to the pool.
+		DBManager.closeConnection(c, rs);
+	}
+	
+	
+	/////////////////////////
+	// Getter Methods
+	/////////////////////////
 	
 	/**
 	 * Getter for name
@@ -29,9 +76,40 @@ public class Category
 		return name;
 	}
 	
+	
+	/**
+	 * Getter for the article count.
+	 * @return The number of articles in this category.
+	 */
+	public int getArticleCount()
+	{
+		return article_count;
+	}
+	
+	
+	/**
+	 * Getter for the set of pages in this category.
+	 * @return A set object with all pages in this category.
+	 */
+	public Set<Page> getPages()
+	{
+		if (pages == null)
+		{
+			retrievePages();
+		}
+		return pages;
+	}
+	
+	
+	/////////////////////////
+	// Overrides
+	/////////////////////////
+	
 	@Override
-	public boolean equals(Object obj) {
-		if (obj instanceof Category) {
+	public boolean equals(Object obj)
+	{
+		if (obj instanceof Category)
+		{
 			Category c = (Category) obj;
 			return this.name.equals(c.name);
 		}
--- Graph_Algorithms/src/graph/Page.java
+++ Graph_Algorithms/src/graph/Page.java
@@ -3,9 +3,7 @@ package graph;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 
@@ -16,8 +14,6 @@ public class Page
 {
 	public int pageId;
 	String pageName;
-	Set<Page> OutgoingLinks;
-	Map<Page,Double> TransitionProbabilities;
 	Set<Category> Categories;
 	
 	/**
@@ -36,71 +32,15 @@ public class Page
 	/////////////////////////
 	
 	/**
-	 * Create a uniform probability distribution over the given pages
-	 * 
-	 * @param outgoingLinks The set of all pages linked to
-	 * @return a mapping of N hyperlinked pages each with 1/N probability
-	 */
-	private void makeUniformProbabilities(Set<Page> outgoingLinks)
-	{
-		TransitionProbabilities = new HashMap<Page, Double>();
-		Double uniform = new Double(1.0/outgoingLinks.size());
-		for (Page p : outgoingLinks)
-		{
-			TransitionProbabilities.put(p, uniform);
-		}
-	}
-	
-	
-	/**
-	 * Handles querying the database for pages which share a category with this one.
-	 */
-	private void retrievePageLinks()
-	{
-		//Run a query to get the page ids linked to by the given one
-		//TODO Do some more sanity checking to make sure we're getting good pages
-		String query =	"SELECT a.page_id AS id " +
-						"FROM articles AS a, categorylinks AS c1, categorylinks AS c2 " +
-						"WHERE c1.cl_to=c2.cl_to AND " +
-						"c2.cl_from=a.page_id AND " +
-						"c1.cl_from=" + pageId + ";";
-		Connection c = DBManager.getConnection();
-		ResultSet rs = DBManager.execute(c, query);
-		if (rs==null){
-			OutgoingLinks = null;
-		}
-		
-		//Populate the return set of pages
-		Set<Page> pages = new HashSet<Page>();
-		try
-		{
-			while(rs.next())
-			{
-				pages.add( new Page(rs.getInt("id")) );
-			}
-		}
-		catch (SQLException e)
-		{
-			e.printStackTrace();
-			OutgoingLinks = null;
-		}
-		
-		//Release the connection back to the pool.
-		DBManager.closeConnection(c, rs);
-		
-		OutgoingLinks = pages;
-	}
-	
-	
-	/**
 	 * Queries the database for this page's title.
 	 */
 	private void retrievePageName()
 	{
-		String query = "SELECT P.page_title as name FROM page as P WHERE P.page_id="+pageId+";";
+		String query = "SELECT p.page_title as name FROM filtered_page as p WHERE p.page_id="+pageId+";";
 		Connection c = DBManager.getConnection();
 		ResultSet rs = DBManager.execute(c, query);
-		if (rs==null){
+		if (rs==null)
+		{
 			pageName = null;
 		}
 		
@@ -125,10 +65,10 @@ public class Page
 	 */
 	private void retrieveCategories()
 	{
-		String query =	"SELECT C.cat_title as Cat " +
-				"FROM categorylinks as CL, category as C " +
-				"WHERE CL.cl_from = "+Integer.toString(pageId) + " " +
-				"AND C.cat_hidden=0 AND C.cat_title=CL.cl_to;";
+		String query =	"SELECT c.cat_title as cat, c.cat_pages as count " +
+				"FROM filtered_categorylinks as cl, filtered_category as c " +
+				"WHERE cl.cl_from = "+Integer.toString(pageId) + " " +
+				"AND c.cat_hidden=0 AND c.cat_title=cl.cl_to;";
 		Connection c = DBManager.getConnection();
 		ResultSet rs = DBManager.execute(c, query);
 		
@@ -140,8 +80,9 @@ public class Page
 			{
 				while(rs.next())
 				{
-					String catName = rs.getString("Cat");
-					Category temp = new Category(catName);
+					String catName = rs.getString("cat");
+					int catCount = rs.getInt("count");
+					Category temp = new Category(catName, catCount);
 					Categories.add(temp);
 				}
 			} catch (SQLException e)
@@ -157,39 +98,6 @@ public class Page
 	/////////////////////////
 	// Getter Methods
 	/////////////////////////
-	
-	/**
-	 * Getter method for transition probabilities of this page
-	 * 
-	 * @return map of Page to its associated transition probability
-	 */
-	public Map<Page,Double> getTransitionProbabilites()
-	{
-		if (TransitionProbabilities == null)
-		{
-			//For now, just using uniform probabilities across all outgoing links
-			makeUniformProbabilities( getOutLinks() );
-		}
-		
-		return TransitionProbabilities;
-	}
-	
-	
-	/**
-	 * Getter method for outgoing links from this page.
-	 * 
-	 * @return A set of Pages linked to by this page.
-	 */
-	public Set<Page> getOutLinks()
-	{
-		if (OutgoingLinks == null)
-		{
-			retrievePageLinks();
-		}
-		
-		return OutgoingLinks;
-	}
-	
 	
 	/**
 	 * Getter for the page title.
